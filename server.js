1e67b092e086bf22b2f48c4976b10bb7874bc436
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2024-06-11 16:34:57 +0300

    update cors policy

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 const PORT = 5000;
 const SECRET_KEY = 'your_secret_key'; // You should use an environment variable for this in production
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // or specify allowed origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
